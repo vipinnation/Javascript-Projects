c543668e9c5497b3b67c04facff16b989f21a380
Vipin Singh Inkiya <62483751+vipinnation@users.noreply.github.com>
2023-01-13 16:48:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+let textArea = document.querySelector("textarea");
+let btn = document.querySelector("button");
+let voiceOption = document.querySelector("select");
+
+let speech = speechSynthesis;
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (textArea.value.trim()) {
+    if (!speech.speaking) {
+      textToSpeech(textArea.value);
+    }
+  }
+});
+
+function textToSpeech(text) {
+  let utterence = new SpeechSynthesisUtterance(text);
+  for (let voice of speech.getVoices()) {
+    if (voice.name == voiceOption.value) {
+      utterence.voice = voice;
+    }
+  }
+  speech.speak(utterence);
+}
+
+function voices() {
+  for (let voice of speech.getVoices()) {
+    let selected = voice.name === "Google हिन्दी" ? "selected" : "";
+    let option = ` <option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+    voiceOption.insertAdjacentHTML("beforeend", option);
+  }
+}
+
+speech.addEventListener("voiceschanged", voices);
